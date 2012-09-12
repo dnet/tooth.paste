40ec0fc99bb8250e789ca1b4b8dbc1d53a8725d6
Maik Röder <mroder@Tahuantinsuyu.local>
2012-09-12 16:03:57 +0200

    add

--- tooth/paste/tooth_basic_namespace.py
+++ tooth/paste/tooth_basic_namespace.py
@@ -1,4 +1,12 @@
 from templer.core.basic_namespace import BasicNamespace
+from templer.core.vars import StringVar
+from templer.core.vars import EASY
+from templer.core.vars import EXPERT
+
+class InvisibleStringVar(StringVar):
+
+    def __repr__(self):
+        return self.default
 
 
 class ToothBasicNamespace(BasicNamespace):
@@ -9,3 +17,30 @@ This creates a Tooth Python project.
 """
     required_templates = []
     use_cheetah = True
+
+    def check_vars(self, vars, cmd):
+        vars = super(BasicNamespace, self).check_vars(vars, cmd)
+        vars['travisci'] =  InvisibleStringVar(
+            'travisci',
+            title='Travis-CI',
+            description='Travis-Ci',
+            default='.travis.ci',
+            modes=(EASY, EXPERT),
+            page='Metadata',
+            help="""
+Travis-CI
+"""
+            )
+
+        vars['zopeskel'] =  InvisibleStringVar(
+            'zopeskel',
+            title='ZopeSkel',
+            description='ZopeSkel',
+            default='.zopeskel',
+            modes=(EASY, EXPERT),
+            page='Metadata',
+            help="""
+ZopeSkel
+"""
+            )
+        return vars
