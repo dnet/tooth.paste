87bbf52b3d18b451cfba2a7c21600c02a4cfcc15
Maik Röder <maikroeder@gmail.com>
2012-09-14 10:22:17 +0200

    =add urls

--- tooth/paste/tooth_basic_namespace.py
+++ tooth/paste/tooth_basic_namespace.py
@@ -1,3 +1,4 @@
+import os
 from templer.core.basic_namespace import BasicNamespace
 from templer.core.vars import StringVar
 from templer.core.vars import EASY
@@ -29,6 +30,66 @@ This creates a Tooth Python project.
             page='Metadata',
             help="""
 Travis-CI
+"""
+            )
+
+        vars['travisci_user'] =  InvisibleStringVar(
+            'travisci_user',
+            title='Travis-CI User',
+            description='Travis-CI User',
+            modes=(EASY, EXPERT),
+            page='Metadata',
+            help="""
+Travis-CI URL
+"""
+            )
+
+        vars['travisci_project'] =  InvisibleStringVar(
+            'travisci_project',
+            title='Travis-CI Project',
+            description='Travis-CI Project',
+            default=os.environ.get('USER', 'travisci_project'),
+            modes=(EASY, EXPERT),
+            page='Metadata',
+            help="""
+Travis-CI Project
+"""
+            )
+
+        info = (vars['travisci_user'], vars['travisci_project'])
+        vars['travisci_url'] =  InvisibleStringVar(
+            'travisci_url',
+            title='Travis-CI URL',
+            description='Travis-CI URL',
+            default="https://secure.travis-ci.org/%s/%s" % info,
+            modes=(EASY, EXPERT),
+            page='Metadata',
+            help="""
+Travis-CI URL
+"""
+            )
+
+        vars['documentation_url'] =  InvisibleStringVar(
+            'documentation_url',
+            title='documentation_url',
+            description='documentation_url',
+            default="",
+            modes=(EASY, EXPERT),
+            page='Metadata',
+            help="""
+documentation_url
+"""
+            )
+
+        vars['repository_url'] =  InvisibleStringVar(
+            'repository_url',
+            title='repository_url',
+            description='repository_url',
+            default="",
+            modes=(EASY, EXPERT),
+            page='Metadata',
+            help="""
+repository_url
 """
             )
 
