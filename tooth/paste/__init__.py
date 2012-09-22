c52766943f564212d0705c0f9d5b98b15f2eb8c6
Maik Röder <maikroeder@gmail.com>
2012-09-22 22:58:26 +0200

    =import new ToothNestedNamespace

--- tooth/paste/__init__.py
+++ tooth/paste/__init__.py
@@ -2,3 +2,4 @@
 This is the tooth.paste Python module.
 """
 from tooth.paste.tooth_basic_namespace import ToothBasicNamespace  # NOQA
+from tooth.paste.tooth_nested_namespace import ToothNestedNamespace  # NOQA
