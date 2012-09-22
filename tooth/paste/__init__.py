b18432c09514c98cfc8b559e4c9c2ad25b2895e4
Maik Röder <maikroeder@gmail.com>
2012-09-22 23:14:35 +0200

    =change import

--- tooth/paste/__init__.py
+++ tooth/paste/__init__.py
@@ -1,5 +1,5 @@
 """
 This is the tooth.paste Python module.
 """
-from tooth.paste.tooth_basic_namespace import ToothBasicNamespace  # NOQA
-from tooth.paste.tooth_nested_namespace import ToothNestedNamespace  # NOQA
+from tooth.paste.basic_namespace import ToothBasicNamespace  # NOQA
+from tooth.paste.nested_namespace import ToothNestedNamespace  # NOQA
