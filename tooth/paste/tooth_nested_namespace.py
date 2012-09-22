e85af0aeea72d30d96643f0cf74d0ba4ec3cf17b
Maik Röder <maikroeder@gmail.com>
2012-09-22 22:59:54 +0200

    =add

---
+++ tooth/paste/tooth_nested_namespace.py
@@ -0,0 +1,21 @@
+"""
+Implement the nested namespace support for tooth.paste, based on Templer.
+"""
+from tooth.paste.tooth_basic_namespace import ToothBasicNamespace
+from templer.core.nested_namespace import VAR_NS2
+
+
+class ToothNestedNamespace(ToothBasicNamespace):
+    _template_dir = 'templates/nested_namespace'
+    summary = "A nested namespace Python package (2 dots in name)"
+    ndots = 2
+    help = """
+This creates a nested namespace Python package with two dots in the name.
+"""
+    required_templates = []
+    use_cheetah = True
+
+    vars = copy.deepcopy(ToothBasicNamespace.vars)
+    get_var(vars, 'namespace_package').default = 'my'
+    vars.insert(2, VAR_NS2)
+    get_var(vars, 'package').default = 'example'
